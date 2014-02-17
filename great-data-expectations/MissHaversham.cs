4eabf0cb3ccd3df3b81bd8e8877f5e41ba646cc5
Andy Cross <andy@elastacloud.com>
2014-02-17 15:58:25 +0000

    force datetimes

--- great-data-expectations/MissHaversham.cs
+++ great-data-expectations/MissHaversham.cs
@@ -9,16 +9,18 @@ namespace GreatExpectations
 {
     public class MissHaversham
     {
-        public static Assertion[] Assert(CloudStorageAccount storageAccount, string containerName, string dataSetPrefix, int minFileExpectation = 1, int maxFileExpectation = 1, string customVariableFormat = "")
+        public static Assertion[] Assert(CloudStorageAccount storageAccount, string containerName, string dataSetPrefix, int minFileExpectation = 1, int maxFileExpectation = 1, string customVariableFormat = "", DateTime? forceStartDateTime = null, DateTime? forceEndDateTime = null)
         {
             // The use of EpochPersistence allows repeated and incremental expectation checking. Be sure to call EpochPersistence.SetLastSatisfied
             // note that this is optional in the workflow and instead arbitrary datetimes can be passed to expectationGenerator.GenerateExpectations
-            var lastExecutionEpoch = EpochPeristence.GetLastSatisfied(storageAccount, containerName);
+            DateTime lastExecutionEpoch, endDateTime;
+            lastExecutionEpoch = forceStartDateTime.HasValue ? forceStartDateTime.Value : EpochPeristence.GetLastSatisfied(storageAccount, containerName);
+            endDateTime = forceEndDateTime.HasValue ? forceEndDateTime.Value : DateTime.Now;
 
             // Create an instance of the ExpectationGenerator; note that this is instance to allow for variant implementations
             // Not yet implemented, but planned for predicatable but not date-oriented generators and different temporal durations
             var expectationGenerator = new ExpectationGenerator();
-            var expectations = expectationGenerator.GenerateExpectations(lastExecutionEpoch, DateTime.Now, dataSetPrefix, minFileExpectation, maxFileExpectation, customVariableFormat);
+            var expectations = expectationGenerator.GenerateExpectations(lastExecutionEpoch, endDateTime, dataSetPrefix, minFileExpectation, maxFileExpectation, customVariableFormat);
 
             // This is a blocking call that iterates over the IAmAnExpection[] and returns Assertions (Results alongside Expectations)
             var assertions = GreatExpectations.Assert.Expectations(expectations, storageAccount, containerName).ToArray();
